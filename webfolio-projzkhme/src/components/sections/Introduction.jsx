35131676c57d789da5273764d53cf40e122c2408
projzkhme <projzkhrrel@gmail.com>
2025-03-31 21:06:01 +0800

    Add Home Page

--- webfolio-projzkhme/src/components/sections/Introduction.jsx
+++ webfolio-projzkhme/src/components/sections/Introduction.jsx
@@ -5,21 +5,17 @@ const roles = ["Developer", "UI / UX Designer", "Editor"];
 
 function Introduction() {
   return (
-    <div className="fixed inset-0 flex flex-col justify-center items-center">
-      <div className="flex flex-col">
-        <div className="text-right">
-          <span className="text-3xl">{name}</span>
-          <div className="flex flex-col opacity-75">
-            {roles.map((role, index) => (
-              <span key={index} className="text-2xl">
-                {role}
-              </span>
-            ))}
-          </div>
-        </div>
-
-        <UILinks />
+    <div className="flex flex-col text-right">
+      <span className="text-3xl">{name}</span>
+      <div className="flex flex-col opacity-75">
+        {roles.map((role, index) => (
+          <span key={index} className="text-2xl">
+            {role}
+          </span>
+        ))}
       </div>
+
+      <UILinks />
     </div>
   );
 }
--- webfolio-projzkhme/src/pages/Home.jsx
+++ webfolio-projzkhme/src/pages/Home.jsx
@@ -2,9 +2,11 @@ import Introduction from "../components/sections/Introduction";
 
 function Home() {
   return (
-    <>
-      <Introduction />
-    </>
+    <div className="fixed inset-0 flex flex-col justify-center items-center">
+      <section>
+        <Introduction />
+      </section>
+    </div>
   );
 }
 export default Home;
